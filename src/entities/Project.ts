04a9c72f1c745c27de08455540b59cf28c63db03
Alireza Safaierad <frontendmonster@gmail.com>
2023-02-21 11:20:03 +0400

    refactor project type

--- src/entities/Project.ts
+++ src/entities/Project.ts
@@ -1,24 +1,11 @@
 import type { IconName } from '.';
 
-export type ProjectName =
-  | 'Arch'
-  | 'CAMP'
-  | 'Charity'
-  | 'English'
-  | 'Estateguru'
-  | 'Germany'
-  | 'GrammarInUse'
-  | 'LifeOS'
-  | 'Neovim'
-  | 'NotionLearning'
-  | 'TPP';
-
 export interface Project {
   icon: IconName;
   id: string;
 }
 
-export const Projects: Record<ProjectName, Project> = {
+export const Projects = {
   CAMP: { id: '49210c9fedbf4fd2812d18b85c5a14c6', icon: 'planet' },
   English: { id: '374871c378fa41be97b55654db151ef3', icon: 'bookmark' },
   Estateguru: { id: 'cf05c40bcea0467bb0816786e9f1f1cc', icon: 'estateguru' },
@@ -30,4 +17,8 @@ export const Projects: Record<ProjectName, Project> = {
   Neovim: { id: '2f541fa3adef4c5aa32980a644f86d8b', icon: 'vim' },
   Arch: { id: 'ea5e41bdd97f4282a31af00c491f31d9', icon: 'arch' },
   Charity: { id: '82529b83e151467b840b9664a2234cc5', icon: 'people' },
-} as const;
+  ASafaeirad: { id: '360055d736504f95a8081ea87fea7be0', icon: 'user-tag' },
+  Einzelganger: { id: '0b7ee1e6d48d416085aeb590738d0962', icon: 'two-circles' },
+} satisfies Record<string, Project>;
+
+export type ProjectName = keyof typeof Projects;
