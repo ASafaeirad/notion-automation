52f72a98d29cf60602e132e7b3928249a5516120
ASafaierad <frontendmonster@gmail.com>
2024-08-10 14:26:41 +0200

    refactor updateIcon

--- src/updateIcons.ts
+++ src/updateIcons.ts
@@ -5,31 +5,21 @@ import { Client } from '@notionhq/client';
 import { config } from './config.js';
 import { Icon } from './entities/Icon.js';
 
-const updateIcon = async () => {
+interface Args {
+  db: string;
+  startCursor?: string;
+  filter?: Parameters<Client['databases']['query']>[0]['filter'];
+}
+
+const updateIcon = async ({ db, startCursor, filter }: Args) => {
   const notionClient = new Client({ auth: config.notionSecret });
   const pages = await notionClient.databases.query({
-    database_id: '83ee0ec8ee524ee5939856e689541b2b',
-    // start_cursor: '8ad04c54-ef82-44aa-8cb5-aaf6d32e2a01',
-    // filter: {
-    //   property: 'Project',
-    //   type: 'relation',
-    //   relation: { contains: '08a493c6-93be-4c3c-b208-8d62d650bf16' },
-    // },
-    // filter: {
-    //   property: 'Status',
-    //   status: { equals: 'Done' },
-    // },
+    database_id: db,
+    start_cursor: startCursor,
+    filter,
   });
 
   const updates = pages.results.map(p => {
-    // const icon = p.icon?.external?.url;
-    // console.log(icon);
-    // if (!icon || icon.includes('/icons/')) return Promise.resolve();
-
-    // const url = `${icon.split('/').slice(0, -1).join('/')}/icons/${icon
-    //   .split('/')
-    //   .pop()}`;
-
     const url = Icon('document');
 
     return notionClient.pages.update({
@@ -41,4 +31,19 @@ const updateIcon = async () => {
   return Promise.all(updates);
 };
 
-await updateIcon();
+const db = '83ee0ec8ee524ee5939856e689541b2b';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const relationFilter = {
+  property: 'Project',
+  type: 'relation',
+  relation: { contains: '08a493c6-93be-4c3c-b208-8d62d650bf16' },
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const statusFilter = {
+  property: 'Status',
+  status: { equals: 'Done' },
+};
+
+await updateIcon({ db });
